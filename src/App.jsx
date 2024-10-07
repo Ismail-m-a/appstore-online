6ddb562d1231f0d062d124e1b4267d4d025f57d4
ismail-m-a <ismaaciilmuxamadaadam@gmail.com>
2024-10-08 00:19:32 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import {ProductStore} from 'kursapp2';
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <ProductStore />
+      
+    </>
+  )
+}
+
+export default App
